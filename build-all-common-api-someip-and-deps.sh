87485e437bb956d6128258c86e87b6bf5a504e0b
Gunnar Andersson <gand@acm.org>
2016-01-21 10:55:37 +0100

    Add comments

--- build-all-common-api-someip-and-deps.sh
+++ build-all-common-api-someip-and-deps.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+# (C) Gunnar Andersson 2016
+# License: CC0
+
+# TIP: Also refer to go.genivi.org for pipelines performing these builds
+
+# Building Common API C++ runtimes and (some) dependencies from source.
+# Not all deps are built from source - there are lower deps that need to be
+# installed on the build machine.  In a Yocto environment you would make sure
+# these deps are built anyhow.
 
 # Input "sleep 1" or whatever you like as first argument
 DELAYCMD="$1"
@@ -10,7 +19,7 @@ set -u
 
 e() {
   "$@"
-  $DELAYCMD
+  $DELAYCMD  # Delay or wait between steps
 }
 
 
@@ -23,10 +32,14 @@ set -x
 git clone --recursive https://github.com/boostorg/boost.git
 git clone http://git.projects.genivi.org/common-api/cpp-someip-runtime.git
 git clone http://git.projects.genivi.org/ipc/common-api-runtime.git
+git clone http://git.projects.genivi.org/ipc/common-api-dbus-runtime.git
 git clone http://git.projects.genivi.org/vSomeIP.git
 git clone http://git.projects.genivi.org/dlt-daemon.git
 git clone https://github.com/doxygen/doxygen.git
 
+# --- DOXYGEN ---
+# Dependencies:
+# * NOTE: Need bison and flex installed (not built from source here)
 e cd "$MYDIR"
 e cd doxygen
 e mkdir -p build
@@ -35,6 +48,8 @@ e cmake -G "Unix Makefiles" -D CMAKE_INSTALL_PREFIX=$PWD/../P/ ..
 e make -j8
 # No need to install - we will make sure the binary is in $PATH
 
+# --- DLT DAEMON ---
+# Dependencies: Not much that I know.  maybe zlib or something simple
 e cd "$MYDIR"
 e cd dlt-daemon
 e mkdir -p build
@@ -63,7 +78,7 @@ e make install  # Need to install pkg-config files for later use
 sed -i 's#${includedir}/dlt#${includedir}#' $PKG_CONFIG_PATH/automotive-dlt.pc
 
 # and libdir also needs fixing.  In this native build libs seem to be installed
-# under ${exec_prefix}/x86_64-linux-gnu/lib, not only ${exec_prefix}/lib ?
+# under ${exec_prefix}/x86_64-linux-gnu/lib, not only ${exec_prefix}/lib !?
 
 # Trying uname to get machine name prefix (x86_64), who knows if this make it
 # more portable maybe:
@@ -72,6 +87,10 @@ correct_libdir="$\{exec_prefix\}/lib/$(uname -m)-linux-gnu"
 # Patch definition of libdir in DLT pkg_ config file
 sed -i "s#libdir=\${exec_prefix}/lib#libdir=$correct_libdir#" $PKG_CONFIG_PATH/automotive-dlt.pc
 
+# --- Common API C++ Runtime ---
+# Dependencies:
+# - OPTIONAL: Doxygen needed for documentation (built from source above)
+# - OPTIONAL: DLT (built from source above)
 e cd "$MYDIR"
 e cd common-api-runtime
 e mkdir -p build
@@ -82,6 +101,8 @@ e grep cmake ../INSTALL | sed 's@^\$@@' | sh
 e make -j8
 # No make install, we just pick the binaries directly from the build directory
 
+# --- BOOST ---
+# Dependencies: Not much.
 e cd "$MYDIR"
 e cd boost
 e git checkout boost-1.58.0
@@ -91,6 +112,11 @@ e ./b2 -j8
 e ./b2 headers
 e ./b2 install
 
+# --- vSomeIP ---
+# Dependencies:
+# - OPTIONAL: Doxygen needed for documentation (built from source above)
+# - Boost (built from source above
+# NOT DLT?  (Uses boost logging?)
 e cd "$MYDIR"
 e cd vSomeIP
 e mkdir -p build
@@ -101,6 +127,23 @@ e cmake ..
 e make -j8
 # No make install, we just pick the binaries directly from the build directory
 
+# --- libdbus with CommonAPI C++ patch ---
+# TODO
+
+# --- Common API C++ D-Bus Runtime ---
+# Dependencies:
+# - libdbus with special patch (built from source above)
+# - Common API C++ (built from source above)
+# - OPTIONAL: DLT (built from source above)
+# TODO
+
+
+# --- Common API C++ Some-IP Runtime ---
+# Dependencies:
+# - vSomeIP (built from source above)
+# - Boost (built from source above
+# - Common API C++ (built from source above)
+# NOT DLT?  (Uses boost logging?)
 e cd "$MYDIR"
 e cd cpp-someip-runtime
 e mkdir -p build
